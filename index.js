0c2118fbcb56a83943194758a7a5cb1bfbc02a58
mjmcgrath2010 <mike@mcgrathsolutions.us>
2018-11-02 16:22:19 -0400

    Adding foundation

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class BaseWidget {
+
+    constructor(hapyak) {
+        this.player = hapyak.context.player;
+    }
+
+    getTime() {
+
+    }
+
+    setTime() {
+
+    }
+ 
+    setPosition() {
+
+    }
+
+
+}
+
+module.exports = BaseWidget;
