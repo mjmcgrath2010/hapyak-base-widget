5ad1aec9cc4c95772697f0b1fa64baa35bde2c3a
mjmcgrath2010 <mike@mcgrathsolutions.us>
2018-11-05 08:31:08 -0500

    Adding other stuff in guy

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 class BaseWidget {
 
     constructor(hapyak) {
-        this.player = hapyak.context.player;
+        this.context = hapyak.context;
+        this.player = hapyak.context && hapyak.context.player;
+        this.currentTime = this.player && this.player.currentTime;
     }
 
     init() {
@@ -15,15 +17,47 @@ class BaseWidget {
     }
 
     getTime() {
-
+        return this.player.currentTime;
     }
 
-    setTime() {
-
+    setTime(time) {
+        return this.player.currentTime(time);
     }
 
-    setPosition() {
+    setPosition(top, left, width, height, duration) {
+        const animateTranisition = (prop, pos) => {
+            let currentPosition;
 
+            if (['top', 'left', 'width', 'height'].indexOf(prop) < 0) {
+                return;
+            }
+            
+            currentPosition = hapyak.context[prop];
+        };
+
+        if (top) {
+            this.context.top = animateTranisition('top', top);
+        }
+
+        if (left) {
+            this.context.left = animateTranisition('left', left);
+        }
+
+        if (width) {
+            this.context.width = animateTranisition('width', width);
+        }
+
+        if (height) {
+            this.context.height = animateTranisition('height', height);
+        }
+    }
+
+    iframeshow() {
+
+    }
+    
+    timeupdate() {
+        this.currentTime = this.player.currentTime;
     }
 
 };
